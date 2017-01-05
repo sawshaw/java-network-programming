445e32a668017c80d85d571cd8b9eb2cfd8b43cc
sawshaw <843506585@qq.com>
2017-01-05 09:27:26 +0800

    client and server

--- networkProgramming/src/serverClient/EchoServer.java
+++ networkProgramming/src/serverClient/EchoServer.java
@@ -10,9 +10,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	private int port=8080;
+	private int port=8000;
 	private ServerSocket serverSocket;
-	public EchoServer() throws Exception{
+	public EchoServer() throws IOException{
 		serverSocket=new ServerSocket(port);
 		System.out.println("服务已启动...");
 	}
@@ -28,17 +28,36 @@ public class EchoServer {
 		return new BufferedReader(new InputStreamReader(socketIn));
 	}
 	public void service(){
+		Socket socket=null;
 		try{
 			while(true){
-				Socket socket=null;
 				socket=serverSocket.accept();
 				System.out.println("new Connecttion accept" +socket.getInetAddress()+":"+socket.getPort());
 				BufferedReader br=getReader(socket);
 				PrintWriter pw=getWriter(socket);
+				String msg=null;
+				while((msg=br.readLine())!=null){
+					System.out.println(msg);
+					pw.println(echo(msg));
+					if(msg.equals("bye")){
+						break;
+					}
+				}
 			}
 		}catch(IOException e){
 			e.printStackTrace();
+		}finally{
+			try{
+				if(socket!=null){
+					socket.close();
+				}
+			}catch(IOException e){
+				e.printStackTrace();
+			}
 		}
+	}
+	public static void main(String[] args)throws IOException{
+		new EchoServer().service();
 	}
 
 }
