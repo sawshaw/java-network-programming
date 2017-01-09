1b5086c35c5fd903d691cfb7c3114abdb34de0bc
sawshaw <843506585@qq.com>
2017-01-09 15:39:23 +0800

    jdk自带线程池实现.....

--- networkProgramming/src/threeServerSocket/threadPool/EchoServer1.java
+++ networkProgramming/src/threeServerSocket/threadPool/EchoServer1.java
@@ -3,22 +3,25 @@ package threeServerSocket.threadPool;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import threeServerSocket.Handler;
 import threeServerSocket.ThreadServer;
 
 /**
  * @author mercy
- * 利用线程池处理客户端请求
+ * 利用jdk自带线程池处理客户端请求
  */
-public class EchoServer {
+public class EchoServer1 {
 	private int port=8000;
 	private ServerSocket serverSocket;
-	private ThreadPool threadPool;
+	private ExecutorService servicePool;//线程池
 	private final int poolSize=5;
-	public EchoServer() throws IOException{
+	public EchoServer1() throws IOException{
 		serverSocket=new ServerSocket(port);
-		threadPool=new ThreadPool(Runtime.getRuntime().availableProcessors()*poolSize);
+		//返回当前电脑系统的CPU数目,CPU越多,线程池工作的数目也越多
+		servicePool=Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors()*poolSize);
 		System.out.println("服务已启动...");
 	}
 	public void service(){
@@ -26,7 +29,7 @@ public class EchoServer {
 			Socket socket=null;
 			try{
 				socket=serverSocket.accept();
-				threadPool.execute(new Handler(socket));//创建一个工线程
+				servicePool.execute(new Handler(socket));//创建一个工线程
 			}catch(IOException e){
 				e.printStackTrace();
 			}
