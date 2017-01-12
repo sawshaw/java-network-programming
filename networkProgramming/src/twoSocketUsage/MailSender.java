3923ec98d7439a5247be35854366c7adbe502951
sawshaw <843506585@qq.com>
2017-01-12 17:54:14 +0800

    改名...

--- networkProgramming/src/twoSocketUsage/MailSender.java
+++ networkProgramming/src/twoSocketUsage/MailSender.java
@@ -1,4 +1,4 @@
-package socketUsageTwo;
+package twoSocketUsage;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- networkProgramming/src/twoSocketUsage/Server.java
+++ networkProgramming/src/twoSocketUsage/Server.java
@@ -1,4 +1,4 @@
-package socketUsageTwo;
+package twoSocketUsage;
 
 import java.io.IOException;
 import java.net.ServerSocket;
